695b39ed6bff11e1f78f4da190c20610f2b49cdb
toluwase <toluwasethomas1@gmail.com>
2022-10-28 12:05:11 +0100

    test create

--- src/main/java/com/example/seerbit/AlgorithmSolutions/Pairs.java
+++ src/main/java/com/example/seerbit/AlgorithmSolutions/Pairs.java
@@ -59,7 +59,4 @@ class MergeIntervals{
 You are given an array (list) of interval pairs as input where each interval has a start
 and end timestamp. The input array is sorted by starting timestamps. You are required
 to merge overlapping intervals and return output array (list).
-
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping
-intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
  */
--- src/test/java/com/example/seerbit/service/serviceimpl/TransactionServiceImplementationTest.java
+++ src/test/java/com/example/seerbit/service/serviceimpl/TransactionServiceImplementationTest.java
@@ -51,5 +51,28 @@ class TransactionServiceImplementationTest {
         assertEquals(BigDecimal.valueOf(12.2243), TransactionRepository.statistics.get("sum"), "Not equal");
     }
 
+    @Test
+    void getStatistics() {
+//        TransactionsDto transactionsDto = new TransactionsDto();
+//        transactionsDto.setAmount(BigDecimal.valueOf(12.3343));
+//        transactionsDto.setTimestamp("2018-07-17T09:59:51.312Z");
+//
+//        Transactions transactions = new Transactions();
+//        transactions.setAmount(transactionsDto.getAmount());
+//        Instant dateValue = Instant.parse(transactionsDto.getTimestamp());
+//        transactions.setTimestamp(Date.from(dateValue));
+//
+//        TransactionRepository.statistics.get("sum");
+        response.setSum(String.valueOf(TransactionRepository.statistics.get("sum").setScale(2, RoundingMode.HALF_UP)));
+        response.setAvg(String.valueOf(TransactionRepository.statistics.get("avg").setScale(2, RoundingMode.HALF_UP)));
+        response.setMin(String.valueOf(TransactionRepository.statistics.get("min").setScale(2, RoundingMode.HALF_UP)));
+        response.setMax(String.valueOf(TransactionRepository.statistics.get("max").setScale(2, RoundingMode.HALF_UP)));
+        response.setCount(String.valueOf(TransactionRepository.statistics.get("count")));
+        transactionService.getStatistics(response);
+        assertEquals(BigDecimal.valueOf(0.00), TransactionRepository.statistics.get("sum"), "Not equal");
+    }
 
+    @Test
+    void deleteStatistics() {
+    }
 }
